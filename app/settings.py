9e98c8eb8c5a8e151a0f53d66fa93168265ee8bc
thiago86.tech <thiago86.tech@gmail.com>
2025-05-24 15:15:42 -0300

    erwer

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # teste
@@ -82,12 +83,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),  # O Render usará essa variável
+        conn_max_age=600,
+        ssl_require=True  # importante para produção
+    )
 }
-
 
 
 # Password validation
